39f888c2bfeabc0eedda16bc20d887f26fd94496
Travis <travis@houseofkrause.org>
2019-07-02 07:56:01 -0500

    update

--- paddle/js/caffeinate.bat
+++
@@ -1,3 +0,0 @@
-rem cmd /c "C:\Dropbox\Programs\CoffeeScript\coffee.exe -b -c "$(CURRENT_DIRECTORY)" -o  "$(CURRENT_DIRECTORY)" && C:\Dropbox\Programs\CoffeeScript\coffee.exe -b -c "$(CURRENT_DIRECTORY)\js" -o  "$(CURRENT_DIRECTORY)\js""
-
-C:\Dropbox\Programs\CoffeeScript\coffee.exe -b -c cd -o  cd
